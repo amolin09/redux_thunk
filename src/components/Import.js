7a2352ccdd2a34f89ff2cfc4e4680fe31c926900
amolin09 <alex.molina5829@gmail.com>
2023-08-30 21:56:23 -0500

    Completed fetch and connections. Stopped at creating a menu for deletion

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import { 
+    Button,
+    Table,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell,
+    Container 
+} from '@mui/material';
+
+import DeleteIcon from '@mui/icons-material/Delete';
+import { MoreVert } from '@material-ui/icons'
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <Container>
+            <h2>Count: {props.makes.length}</h2>
+            <Button variant="contained" color="primary" onClick={props.fetchMakes}>Import</Button>
+
+            <Table>
+                <TableHead>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Make</TableCell>
+                    <TableCell>Vehicle Type</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableHead>
+                <TableBody>
+                    {props.makes.map((make, idx) => (
+                        <TableRow key={make.MakeId}>
+                            <TableCell>{make.MakeId}</TableCell>
+                            <TableCell>{make.MakeName}</TableCell>
+                            <TableCell>{make.VehicleTypeName}</TableCell>
+                            <DeleteIcon color="secondary"
+                            onClick={() => props.removeMake(idx)}
+                            />
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Container>
     )
 }
 
--- src/containers/Import.js
+++ src/containers/Import.js
@@ -1,1 +1,19 @@
 // fill out this container
+import { connect } from 'react-redux';
+import Import from '../components/Import';
+import { fetchMakes, removeMake } from '../redux/actions';
+
+const mapStateToProps = (state) => {
+  return{
+    makes: state.makes || []
+  }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    removeMake: (index) => dispatch(removeMake(index)),
+    fetchMakes: () => dispatch(fetchMakes())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Import)
